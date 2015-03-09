5134456b7969454057b29e9ec3b8f7afa5013f14
wono <a@wono.me>
2015-03-08 18:05:59 -0700

    inlined css for escape globally changed css style from last merge
    Signed-off-by: wono <a@wono.me>

--- application/views/admin/main.php
+++ application/views/admin/main.php
@@ -1,9 +1,10 @@
-
 <div id="body">
+    <br/>
+    <br/>
     <p>User Lists</p>
     <ul>
     {userlists}
-    <li><a href="/admin/edit_user_profile/{id}">{first_name} {last_name}</a></li>
+    <li><a href="/admin/edit_user_profile/{id}" style="color:blue;">{first_name} {last_name}</a></li>
     {/userlists}
     </ul>
 </div>
